b576f8a6d160e07891575e1bd31aa0e983e227dc
mix irving <whimful@gmail.com>
2016-07-09 20:21:58 +1200

    working inu mvp

--- client/App.js
+++ client/App.js
@@ -3,10 +3,7 @@ import defer from 'pull-defer'
 import request from 'xhr'
 
 const INITIAL_MODEL = {
-  pairings: [{
-    Player: 'dave schoze',
-    Table: 12,
-  }]
+  pairings: []
 }
 
 const FETCH_PAIRINGS = 'FETCH_PAIRINGS'
@@ -15,23 +12,24 @@ const UPDATE_PAIRINGS = 'UPDATE_PAIRINGS'
 const App = {
   init: () => ({
     model: INITIAL_MODEL,
-    //effect: FETCH_PAIRINGS
+    effect: { type: FETCH_PAIRINGS }
   }),
 
-  //update: (model, action) => {
-    //switch (action.type) {
-      //case UPDATE_PAIRINGS:
-        //const parings = action.pairings
-        //const model = Object.assign({}, model, { pairings })
-        //return { model }
-      //default:
-        //return model
-    //}
-  //},
+  update: (model, action) => {
+
+    switch (action.type) {
+      case UPDATE_PAIRINGS:
+        const pairings = action.pairings
+        const model = { pairings }
+        return { model }
+      default:
+        return model
+    }
+  },
 
   view: (model, dispatch) => html`
     <div class='main'>
-      <p>Hi</p>
+      <h1>MTG table pairings</h1>
       ${model.pairings.map( p => html` 
           <div class='row'>
             <div class='ten columns'>${p.Player}</div>
@@ -42,30 +40,32 @@ const App = {
     </div>
   `,
 
-  //run: (effect) => {
-    //switch (effect.type) {
+  run: (effect) => {
+    switch (effect.type) {
 
-      //case FETCH_PAIRINGS:
-        //let source = defer.source()
+      case FETCH_PAIRINGS:
+        let source = defer.source()
 
-        //request.get('/data', (err, res) => {
-          //if (err) console.log(err)
+        request.get('/data', (err, res) => {
+          if (err) console.log(err)
 
-          //const model = JSON.parse(res.body)
-          //console.log('model in client bundle', model)
-          //const pairings = model.pairings
+          const model = JSON.parse(res.body)
+          const pairings = model.pairings
 
-          //source.resolve(
-            //pull.values({
-              //type: UPDATE_PAIRINGS,
-              //pairings
-            //})
-         //)
-        //})
+          source.resolve(
+            pull.values([{
+              type: UPDATE_PAIRINGS,
+              pairings
+            }])
+         )
+        })
 
-        //return source
-    //}
-  //}
+        return source
+
+      default:
+        console.log('some unmatched effect', effect)
+    }
+  }
 }
 
 export default App
